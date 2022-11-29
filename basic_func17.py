146db584f0236de9ce39df1de4d9156e5bf37b9a
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-29 20:54:00 +0500

    done

--- basic_func17.py
+++ basic_func17.py
@@ -5,4 +5,3 @@ from math import pi
 def main(a):
     a=pi
     return a
-print(main(pi))
--- basic_func18.py
+++ basic_func18.py
@@ -5,5 +5,5 @@
 from math import pi
 def main(a):
     a=pi
-    return (round(a,2))
-print(main(a))
+    return round(a,2)
+
